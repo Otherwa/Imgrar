1b4d68cf5a6bdf41855a9a24a55cb563d1e12500
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2023-06-01 18:34:20 +0530

    /package done/

--- in/baseconversion.py
+++ in/baseconversion.py
@@ -1,37 +1,46 @@
 from PIL import Image
 import os
+import math
 from time import sleep
 
 
-def factors(n):
+def convert_to_grid(number):
     factors = []
-    for i in range(1, n + 1):
-        if (n % i) == 0:
-            factors.append(i)
-    return factors
+    for i in range(1, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            factors.append((i, number // i))
+    rows, columns = factors[-1]  # Get the factors closest together
+    return rows, columns
+
+
+def add_white_spaces(num_str, frame_size: int):
+    num_len = len(num_str)
+    if num_len % frame_size != 0:
+        white_spaces = bytes("\u0020" * (frame_size - (num_len % frame_size)), "utf-8")
+        num_str += white_spaces
+    return num_str
 
 
 class baseconversion:
-    def __init__(self, inputfolder, outputfolder):
+    def __init__(self, inputfolder, outputfolder, frame_size="320 * 240"):
         self.infolder = inputfolder
         self.outfolder = outputfolder
-
+        self.frame_size = int(eval(frame_size))
         # make folder
         os.makedirs(self.outfolder, exist_ok=True)
         os.makedirs(self.infolder, exist_ok=True)
 
     def code2png(self):
-        print("Code 2 png binary\n")
+        print("Code 2 Photo\n")
         for filename in os.listdir(self.infolder):
             filepath = os.path.join(self.infolder, filename)
             with open(filepath, "r") as f:
                 data = f.read()
             binary_data = bytes(data, "utf-8")
             print(f"File Bytes : {len(binary_data)}")
-            sizes = factors(len(binary_data))
-            img = Image.frombytes(
-                "L", (sizes[len(sizes) // 2 - 1], sizes[len(sizes) // 2]), binary_data
-            )
+            binary_data = add_white_spaces(binary_data, self.frame_size)
+            cols, rows = convert_to_grid(len(binary_data))
+            img = Image.frombytes("L", (rows, cols), binary_data)
             print(f"Image dim : {img.size}")
             img.save(os.path.join(self.outfolder, filename) + ".png")
             print(filename + " ✅\n", end="\n")
@@ -39,10 +48,9 @@ class baseconversion:
         print("\nDone 🤖✅")
 
     def png2code(self):
-        print("Png 2 Code Binary")
+        print("Photo 2 Code\n")
         for filename in os.listdir(self.outfolder):
-            filepath = os.path.join(self.outfolder, filename)
-            img = Image.open(filepath)
+            img = Image.open(os.path.join(self.outfolder, filename))
             binary_data = img.tobytes()
             text_data = binary_data.decode("utf-8")
             file = filename.split(".")
@@ -51,5 +59,3 @@ class baseconversion:
                 f.writelines(text_data)
             print(f"Image dim : {img.size}")
             print(filename + " ✅\n", end="\n")
-            print(filepath + " Done ✅")
-  
--- src/baseconversion.py
+++ src/baseconversion.py
@@ -9,25 +9,23 @@ def convert_to_grid(number):
     for i in range(1, int(math.sqrt(number)) + 1):
         if number % i == 0:
             factors.append((i, number // i))
-
-    print(factors)
     rows, columns = factors[-1]  # Get the factors closest together
     return rows, columns
 
 
-def add_white_spaces(num_str):
+def add_white_spaces(num_str, frame_size: int):
     num_len = len(num_str)
-    if num_len % 10 != 0:
-        white_spaces = bytes(" " * (10 - (num_len % 10)), "utf-8")
+    if num_len % frame_size != 0:
+        white_spaces = bytes("\u0020" * (frame_size - (num_len % frame_size)), "utf-8")
         num_str += white_spaces
     return num_str
 
 
 class baseconversion:
-    def __init__(self, inputfolder, outputfolder):
+    def __init__(self, inputfolder, outputfolder, frame_size="320 * 240"):
         self.infolder = inputfolder
         self.outfolder = outputfolder
-
+        self.frame_size = int(eval(frame_size))
         # make folder
         os.makedirs(self.outfolder, exist_ok=True)
         os.makedirs(self.infolder, exist_ok=True)
@@ -40,9 +38,7 @@ class baseconversion:
                 data = f.read()
             binary_data = bytes(data, "utf-8")
             print(f"File Bytes : {len(binary_data)}")
-            binary_data = add_white_spaces(binary_data)
-            # add white space to adjust the rxc
-
+            binary_data = add_white_spaces(binary_data, self.frame_size)
             cols, rows = convert_to_grid(len(binary_data))
             img = Image.frombytes("L", (rows, cols), binary_data)
             print(f"Image dim : {img.size}")
